34609e05225cdc27a05685aae3077e3e1c6918e0
F <samsunga137fds@gmail.com>
2026-08-14 21:58:45 +0300

    feat: add Drizzle migration configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    host: process.env.SQL_HOST ?? '',
+    user: process.env.SQL_USER ?? '',
+    password: process.env.SQL_PASSWORD ?? '',
+    database: process.env.SQL_DB_NAME ?? '',
+    port: Number(process.env.SQL_PORT ?? 5432),
+  },
+});
