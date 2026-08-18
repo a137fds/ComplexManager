c33824710fe5e316f9db6293c492c4cd730a8b19
F <samsunga137fds@gmail.com>
2026-08-18 08:44:53 +0300

    Load translation normalization before app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './auth/AuthContext';
 import { AuthGate } from './auth/AuthGate';
+import './i18n/translationPatches';
 import './index.css';
 
 const rootElement = document.getElementById('root');
