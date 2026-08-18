8ea4ebaf9f0cd2734e6b5867d30986d2b75bddc5
F <samsunga137fds@gmail.com>
2026-08-18 08:44:44 +0300

    Normalize mixed-language translation labels

---
+++ src/i18n/translationPatches.ts
@@ -0,0 +1,30 @@
+import { translations } from './translations';
+
+// Keep only explicitly agreed original Turkish terms such as (Site) and (Kapıcı).
+translations.ru.navComplex = 'Жилой комплекс (Site)';
+translations.tr.navComplex = 'Site (Site)';
+translations.ru.tagline = 'Система управления жилым комплексом (Site) в Турции';
+
+translations.ru.roleAdmin = 'Администратор';
+translations.ru.roleManagementCompany = 'Управляющая компания';
+translations.ru.roleChairman = 'Председатель правления';
+translations.ru.roleBoardMember = 'Член правления';
+translations.ru.roleFinancialController = 'Финансовый контролёр';
+translations.ru.roleResident = 'Жилец';
+translations.ru.roleGuest = 'Гость';
+
+translations.ru.softDelete = 'Пометить на удаление (архив)';
+translations.ru.sendToAll = 'Отправить всем жильцам';
+translations.ru.boardApproval = 'Утверждение правлением';
+translations.ru.taxInvoiceAttached = 'Приложен официальный турецкий электронный счёт (e-Fatura)';
+translations.ru.auditTrail = 'Журнал аудита действий системы';
+
+translations.tr.sendToAll = 'Tüm Sakinlere Gönder';
+translations.da.sendToAll = 'Send til alle beboere';
+translations.ru.auditCorrect = 'Проверка пройдена (без замечаний)';
+
+translations.en.navComplex = 'Complex (Site)';
+translations.fr.navComplex = 'Résidence (Site)';
+translations.da.navComplex = 'Kompleks (Site)';
+translations.sv.navComplex = 'Bostadskomplex (Site)';
+translations.pl.navComplex = 'Kompleks (Site)';
